fc05ba380ed8d1878c53dba5aada0ba5cec8c369
Dakota Jones <dakota32jones@gmail.com>
2022-07-08 17:41:27 -0500

    added questions array for javascript

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,10 +1,41 @@
 var questions = [
   {
-    questionText: "",
+    questionText: "Inside which element do we put the Javascript?",
 
-    choices: [],
+    choices: ["<javascript>", "<js>", "<script>", "<scripting>"],
 
-    answer: "",
+    answer: "<script>",
+  },
+
+  {
+    questionText: "How do you call a function named myFunction?",
+
+    choices: [
+      "call myFunction()",
+      "call function",
+      "Call.myfunction()",
+      "myFunction()",
+    ],
+
+    answer: "myFunction()",
+  },
+
+  {
+    questionText:
+      "Code that will not be executed but is instead used to describe what your code does is called?",
+
+    choices: ["comments", "quotes", "annotations", "remarks"],
+
+    answer: "comments",
+  },
+
+  {
+    questionText:
+      "Which of the following is the equality operator that determines that two variables are equal if they are of the same type and have the same value?",
+
+    choices: ["=", "===", "==", "!=="],
+
+    answer: "===",
   },
 
   // make sure to do an array of objects for each question you want in your game!
