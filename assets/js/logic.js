01fd4cd3cda9fa22d7b5605ec1d827fc8d7e6b07
Dakota Jones <dakota32jones@gmail.com>
2022-07-10 17:39:01 -0500

    added buttons to show when starting the quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,28 +6,45 @@ var questionsEl = document.querySelector("#questions");
 var startBtn = document.querySelector("#start-btn");
 var feedbackEl = document.querySelector("#feedback");
 var initialsEl = document.querySelector("#initials");
+var QuestionTitle = document.querySelector("#question-title");
+var endScreen = document.querySelector("#end-screen");
+var startScreen = document.querySelector("#start-screen");
+var timeRemaining = 5;
+var timer;
+var currentQuestion = 0;
 
-// quiz variables
+// setting a countdown timer for the game
+function countdown() {
+  timeRemaining--;
 
-var timerId;
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-
-// hide the start
-
+  timerEl.textContent = parseInt(timeRemaining);
+  if (timeRemaining <= 0) {
+    clearInterval(timer);
+  }
+}
+// once the user clicks start quiz the start screen disappears and the counter start counting down.
 function startQuiz() {
-  var startScreenEl = document.getElementById("start-screen");
-  startScreenEl.setAttribute("class", "hide");
+  startScreen.style.display = "none";
+  timerEl.textContent = timeRemaining;
+  timer = setInterval(countdown, 1000);
+  renderQuestion();
+  renderAnswers();
+}
+startBtn.addEventListener("click", startQuiz);
 
-  //   after starting the starting screen we need to reveal the questions.
+// trying to render the questions to the screen.
+function renderQuestion() {
+  var questionToRender = questions[currentQuestion];
+  questionsEl.textContent = questionToRender.questionText;
+}
 
-  questionsEl.removeAttribute("class");
-
-  // once questions are revealed we now, reveal the timer to countdown
-
-  timerId = setInterval(clockTick, 1000);
-
-  timerEl.textContent = time;
-
-  getQuestion();
+// create buttons for the choices on the screen.
+function renderAnswers() {
+  var question = questions[currentQuestion];
+  var choicesToRender = question.choices;
+  Object.values(choicesToRender).forEach(function (choice, index) {
+    var choicesEl = document.createElement("button");
+    choicesEl.textContent = choice;
+    questionsEl.appendChild(choicesEl);
+  });
 }
---
+++ assets/js/scores.js
@@ -0,0 +1,1 @@
+// save and clear high scores and retrieve scores from local storage
